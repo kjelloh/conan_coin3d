b0dc0e4171440eacaebf97b23fe09448534655d1
kjelloh <kjell-olov.hogdahl@itfied.se>
2025-03-02 18:08:53 +0100

    Initial local commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # conan_coin3d
 A Conan package of the  Coin3D C++ library
+
+250302 - git clone in conanfile.py fails with 'RPC failed; curl 92 HTTP/2 stream 5 was not closed cleanly'
+         But: git clone from shell in macOS works fine?
+
+## How this project was setup
+
+
+## Coin3D version Coin 4.0.3
+
+```sh
+conan new cmake_lib -d name=coin3d -d version=4.0.3
+
+File saved: CMakeLists.txt
+File saved: conanfile.py
+File saved: include/coin3d.h
+File saved: src/coin3d.cpp
+File saved: test_package/CMakeLists.txt
+File saved: test_package/conanfile.py
+File saved: test_package/src/example.cpp
+
+```
---
+++ conanfile.py
@@ -0,0 +1,58 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+from conan.tools.scm import Git
+
+class coin3dRecipe(ConanFile):
+    name = "coin3d"
+    version = "4.0.3"
+    package_type = "library"
+
+    # Optional metadata
+    license = "<Put the package license here>"
+    author = "<Put your name here> <And your email here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of coin3d package here>"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    # Sources are located in the same place as this recipe, copy them to the recipe
+    exports_sources = "CMakeLists.txt", "src/*", "include/*"
+
+    def source(self):
+        git = Git(self)
+        git.run("clone https://github.com/coin3d/coin.git")
+        git.run("checkout 8f19fe933040bbbe74dee474ad2231291b9b306e")
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            self.options.rm_safe("fPIC")
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+
+    def layout(self):
+        cmake_layout(self)
+    
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["coin3d"]
+
